a12fd833475c6f908d67040d538d38f00aff968f
Frank Denis <github@pureftpd.org>
2024-09-18 18:27:38 +0200

    Import the dnscry.pt-merge scripts

---
+++ utils/dnscry.pt-merge.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+# Origin: https://www.dnscry.pt/resolvers.md
+
+import sys
+
+
+servers = {}
+
+in_header = True
+
+with sys.stdin as f:
+    while True:
+        head = f.readline()
+        if not head:
+            break
+        if in_header == True:
+            if head.startswith("## "):
+                in_header = False
+            else:
+                continue
+        if head.startswith("## "):
+            name = head.split("## ")[1]
+            name = f"dnscry.pt-{name}".replace(" ", "")
+            print(f"## {name}")
+        else:
+            print(head)
